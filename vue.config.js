39c2ac154d7c7a10865cda542486a6dc943ab626
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-07-20 21:45:02 -0300

    init

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const globalSassFiles = ["./src/sass/base/_variables.scss"];
+
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  publicPath: './',
+  lintOnSave: false,
+
+  chainWebpack: (config) => {
+    const oneOfsMap = config.module.rule("scss").oneOfs.store;
+    oneOfsMap.forEach((item) => {
+      item
+        .use("sass-resources-loader")
+        .loader("sass-resources-loader")
+        .options({
+          resources: globalSassFiles,
+        })
+        .end();
+    });
+  },
+};
